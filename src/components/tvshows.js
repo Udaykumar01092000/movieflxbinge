1e9759ffda23012f74d4f5ba53ffe7e22ad301b0
Uday Kumar <udaykumar.77348@gmail.com>
2024-08-10 13:17:07 +0530

    Add environment variables and update API configuration

--- src/components/tvshows.js
+++ src/components/tvshows.js
@@ -19,15 +19,7 @@ function InitialMovies() {
       return rej;
     })
   },[])
-    
-  const formatDate = (dateString) => {
-        const date = new Date(dateString);
-        const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
-        const year = date.getFullYear();
-        return `${day}-${month}-${year}`;
-    };
-
+  
     console.log(tvShows);
     return (
       <div>
